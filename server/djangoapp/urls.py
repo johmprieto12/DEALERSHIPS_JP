40a8337b91c599b986ec329846053482b6bc03d0
Johm Prieto <johmprieto@gmail.com>
2025-12-09 22:49:08 -0500

    dealer details or reviews page

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     # path for get cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for get dealer 
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path fot get dealer details
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
